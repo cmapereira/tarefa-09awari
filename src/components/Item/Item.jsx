da45491598d2fb631f30392c14418041890272f6
cmapereira <cmapereira@gmail.com>
2023-03-16 11:53:42 -0300

    firt commit

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,14 @@
+import { useCallback } from 'react';
+
+const Item = ({ item, onItemClick }) => {
+  const handleClick = useCallback(() => {
+    onItemClick && onItemClick(item);
+  }, [item, onItemClick]);
+  return (
+    <li>
+      <p>{item.name}</p>
+      <button type="button" onClick={handleClick}>Ver detalhes</button>
+    </li>
+  );
+};
+export default Item;
